ff56acfddc5fb54f6bd552cb9fbe6912e9470fc7
Anthony Doan-Ha <adoanha@ucla.edu>
2024-03-12 22:19:30 -0700

    Working app to change from light mode to dark mode, or vice-versa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const darkTheme = () => {
+    document.documentElement.style.setProperty("--bg-color", "#222");
+    document.documentElement.style.setProperty("--text-primary", "#fff");
+    document.documentElement.style.setProperty("--text-secondary", "#ddd");
+  };
+
+  const lightTheme = () => {
+    document.documentElement.style.setProperty("--bg-color", "#fff");
+    document.documentElement.style.setProperty("--text-primary", "#000");
+    document.documentElement.style.setProperty("--text-secondary", "#333");
+  };
+
+  document.getElementById("dark", darkTheme);
+  document.getElementById("light", lightTheme);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Title</h1>
+      <p className="content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid,
+        tenetur provident eaque ipsum unde vel quisquam minima maiores nobis aut
+        inventore atque neque, sed quae at asperiores laborum. Exercitationem,
+        harum?
+      </p>
+      <div className="btn__container">
+        <button id="dark" className="btn__dark" onClick={darkTheme}>
+          Dark Theme
+        </button>
+        <button id="light" className="btn__light" onClick={lightTheme}>
+          Light Theme
+        </button>
+      </div>
     </div>
   );
 }
